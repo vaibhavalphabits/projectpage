facb3a20a91a6cdba80db9011be7b5aa346aedfa
vaibhavalphabits <vaibhav@alphabitssolutions.com>
2024-09-27 17:50:09 +0530

    My Commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -26,23 +26,23 @@ export default function Dashboard() {
           alt="Project Image"
         />
         <ProjectCard
-          imageURL="https://images.pexels.com/photos/28232669/pexels-photo-28232669/free-photo-of-autumn-garden-vibe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          imageURL="https://images.pexels.com/photos/18923568/pexels-photo-18923568/free-photo-of-teapot-on-a-tree-trunk.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Project Image"
         />
         <ProjectCard
-          imageURL="https://images.pexels.com/photos/28232669/pexels-photo-28232669/free-photo-of-autumn-garden-vibe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          imageURL="https://images.pexels.com/photos/9700693/pexels-photo-9700693.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="Project Image"
         />
         <ProjectCard
-          imageURL="https://images.pexels.com/photos/28232669/pexels-photo-28232669/free-photo-of-autumn-garden-vibe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          imageURL="https://images.pexels.com/photos/9704634/pexels-photo-9704634.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="Project Image"
         />
         <ProjectCard
-          imageURL="https://images.pexels.com/photos/28232669/pexels-photo-28232669/free-photo-of-autumn-garden-vibe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          imageURL="https://images.pexels.com/photos/227429/pexels-photo-227429.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="Project Image"
         />
         <ProjectCard
-          imageURL="https://images.pexels.com/photos/28232669/pexels-photo-28232669/free-photo-of-autumn-garden-vibe.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          imageURL="https://images.pexels.com/photos/28085843/pexels-photo-28085843/free-photo-of-the-island-of-montenegro-is-surrounded-by-blue-water.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Project Image"
         />
       </div>
